5597da9d015923f985788ae83c29dfb54c96c8a7
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-08 13:57:49 -0700

    Add draw function.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-function draw (e, isDrawing) {
-    if (!isDrawing) {console.log(isDrawing); return;}
-    console.log(e);
-    console.log(isDrawing);
+function draw (e, isDrawing, ctx) {
+    if (!isDrawing) return;
+    let lastX, lastY;
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(e.lastX, e.lastY);
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
 }
 
 (function(){
@@ -11,15 +18,13 @@ function draw (e, isDrawing) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     // Ref: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
-    ctx.strokeStyle = '#fff';
+    ctx.strokeStyle = '#111';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
 
     let isDrawing = false;
-    let lastX = 0;
-    let lastY = 0;
 
-    canvas.addEventListener('mousemove', (e) => draw(e, isDrawing));
+    canvas.addEventListener('mousemove', (e) => draw(e, isDrawing, ctx));
     canvas.addEventListener('mousedown', () => isDrawing = true);
     canvas.addEventListener('mouseup', () => isDrawing = false);
     canvas.addEventListener('mouseout', () => isDrawing = false);
