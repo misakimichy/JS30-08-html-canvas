d34c15e94d7c1a5a600dbd815e014020e7315951
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-08 23:44:03 -0700

    Fix drawing line function to set defaylt of mouse and lastmouse of X and Y.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
-function draw (e, isDrawing, ctx) {
-    if (!isDrawing) return;
-    let lastX, lastY;
-    ctx.beginPath();
-    // start from
-    ctx.moveTo(e.lastX, e.lastY);
-    lastX = e.offsetX;
-    lastY = e.offsetY;
-    // go to
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-}
-
 (function(){
     const canvas = document.querySelector("#draw");
     const ctx = canvas.getContext('2d');
     // make width and height the window size
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    // Ref: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
-    ctx.strokeStyle = '#111';
+
+    const mouse = {x: 0, y: 0};
+    const lastMouse = {x: 0, y: 0};
+
+    ctx.lineWidth = 20;
+    ctx.strokeStyle = '#fc9803';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
 
-    let isDrawing = false;
+    canvas.addEventListener('mousemove', (e) => {
+        lastMouse.x = mouse.x;
+        lastMouse.y = mouse.y;
+        mouse.x = e.pageX - e.currentTarget.offsetLeft;
+        mouse.y = e.pageY - e.currentTarget.offsetTop;
 
-    canvas.addEventListener('mousemove', (e) => draw(e, isDrawing, ctx));
-    canvas.addEventListener('mousedown', () => isDrawing = true);
-    canvas.addEventListener('mouseup', () => isDrawing = false);
-    canvas.addEventListener('mouseout', () => isDrawing = false);
+        let color = e.x;
+        ctx.strokeStyle = `hsl(${color}, 100%, 50%)`;
+    }, false);
+
+    canvas.addEventListener('mousedown', () => {
+        canvas.addEventListener('mousemove', draw, false);
+    }, false);
+    canvas.addEventListener('mouseup', () => {
+        canvas.removeEventListener('mousemove', draw, false)
+    }, false);
+    canvas.addEventListener('mouseout', () => {
+        canvas.removeEventListener('mousemove', draw, false);
+    }, false);
+
+    const draw = () => {
+        ctx.beginPath();
+        ctx.moveTo(lastMouse.x, lastMouse.y);
+        ctx.lineTo(mouse.x, mouse.y);
+		ctx.closePath();
+		ctx.stroke();
+    };
 }());
